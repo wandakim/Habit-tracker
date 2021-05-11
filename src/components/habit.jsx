d735d6ba829378bbe1f47e4f033a2e3747ea3a2c
wandakim <nwd1583@naver.com>
2021-05-11 20:34:52 +0900

    habit componanents.

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -8,18 +8,27 @@ class Habit extends Component {
     // state object 안에 있는 count를 증가시키고 state를 업데이트 해야 함.
     this.setState({ count: this.state.count + 1 });
   };
+  handledecrement = (e) => {
+    // state object 안에 있는 count를 증가시키고 state를 업데이트 해야 함.
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleincrement}
         >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handledecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
