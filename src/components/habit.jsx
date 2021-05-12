0c7461ed150746d72d951a231c03987b5a65fcf0
wandakim <nwd1583@naver.com>
2021-05-12 12:59:50 +0900

    event update functions

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
   };
-  handleincrement = (e) => {
-    // state object 안에 있는 count를 증가시키고 state를 업데이트 해야 함.
-    this.setState({ count: this.state.count + 1 });
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
   };
-  handledecrement = (e) => {
-    // state object 안에 있는 count를 증가시키고 state를 업데이트 해야 함.
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
+
   render() {
     const { name, count } = this.props.habit;
     return (
@@ -21,17 +19,20 @@ class Habit extends Component {
         <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
-          onClick={this.handleincrement}
+          onClick={this.handleIncrement}
         >
           <i className="fas fa-plus-square"></i>
         </button>
         <button
           className="habit-button habit-decrease"
-          onClick={this.handledecrement}
+          onClick={this.handleDecrement}
         >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
